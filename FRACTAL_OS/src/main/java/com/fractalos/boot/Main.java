a24d4c8d0aca22a7cecfae533d503a526d9e9b83
VladyThrash <vladyepcon@gmail.com>
2026-06-03 20:56:51 -0600

    Primer commit :)

---
+++ FRACTAL_OS/src/main/java/com/fractalos/boot/Main.java
@@ -0,0 +1,8 @@
+package com.fractalos.boot;
+
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hola equipo :)");
+        System.out.println("Este es el primer pull en Github :O");
+    }
+}
