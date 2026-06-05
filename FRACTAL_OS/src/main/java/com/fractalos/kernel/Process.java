75e401caa1cf4478c2c07342d799a4cdea97ef05
VladyThrash <vladyepcon@gmail.com>
2026-06-05 13:34:42 -0600

    Modificando Proccess, ahora es compatible con el paquete java.util.concurrent

--- FRACTAL_OS/src/main/java/com/fractalos/kernel/Process.java
+++ FRACTAL_OS/src/main/java/com/fractalos/kernel/Process.java
@@ -2,7 +2,10 @@ package com.fractalos.kernel;
 
 //Bloque de control de proceso (PCB).
 //Esta clase define y almacena toda la información de un hilo.
-public class Process {
+//Implementamos Runnable para que el ExecutorService (CPU) lo entienda.
+//Implementamos Comparable para que la PriorityQueue (Scheduler) lo ordene.
+
+public class Process  implements Runnable, Comparable<Process>{
 
     //Posibles estados en los que se puede encontrar un proceso.
     public enum STATE{
@@ -59,4 +62,35 @@ public class Process {
         this.remProcessing = remProcessing;
     }
 
+    //Sobreecribimos el método de la interfaz funcional Runnable.
+    @Override
+    public void run() {
+        this.setActualState(STATE.RUNNING_PROCESS);
+
+        System.out.println("CPU ejecutando Proceso PID: " + this.pid);
+
+        //Simulamos la ejecución del ciclo.
+        while (remProcessing > 0) {
+            // Simulamos trabajo y tiempo que toma un ciclo de CPU
+            try {
+                Thread.sleep(100); // 100ms por ciclo
+            } catch (InterruptedException e) {
+                // Si el planificador interrumpe el hilo (Expropiación/Preemption)
+                this.setActualState(STATE.READY_PROCESS);
+                System.out.println("Proceso PID: " + this.pid + " fue interrumpido.");
+                return; // Sale de la CPU
+            }
+            remProcessing--;
+        }
+
+        this.setActualState(STATE.FINISHED_PROCESS);
+        System.out.println("Proceso PID: " + this.pid + " finalizó su ráfaga.");
+    }
+
+    //Sobreescribimos el método de la clase funcional Comparable.
+    @Override
+    public int compareTo(Process otherProcess) {
+        // Ordena de mayor a menor prioridad
+        return Integer.compare(otherProcess.getPriority(), this.priority);
+    }
 }
